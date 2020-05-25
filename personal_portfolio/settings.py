737662ef69745c0385daabfc7b12fdb6e78901bd
Thaddeus Martin <martin.p.thaddeus@gmail.com>
2020-05-25 15:45:25 -0700

    requirement file added

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'd9&*w_x7brcc#m_tcal$3g=%9y0g(1_(_xoi$^zixfe8y=ic=9'
 DEBUG = False
 
 ALLOWED_HOSTS = ['acuitytpm.pythonanywhere.com',
-                    'thaddeus.dev']
+    'thaddeus.dev']
 
 
 # Application definition
@@ -128,3 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file found")
